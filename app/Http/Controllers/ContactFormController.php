08afc2dcc3f0db79f428c128a61f29c6f5d087d7
jbratcher <jeremybratcher@gmail.com>
2019-09-08 14:33:36 -0400

    upgraded to laravel 6

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\ContactEmail;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class ContactFormController extends Controller
@@ -27,19 +28,28 @@ class ContactFormController extends Controller
         $email->message = $request->message;
         $email->save();
 
+        // get last contact form submitted as last contact email row in table
+
+        $latest_email = DB::table('contact_emails')->latest->first();
+
         //  Add mail functionality here.
 
-        try
-        {
-            Mail::send($email);
-            print("Message sent successfully");
-        }
-        catch(Exception $e)
-        {
-            print("Message not sent");
-        }
+        // try
+        // {
+        //     Mail::send($email);
+        //     print("Message sent successfully");
+        // }
+        // catch(Exception $e)
+        // {
+        //     print("Message not sent");
+        // }
 
+        $json = response()->json(null, 200);
 
-        return response()->json(null, 200);
+        print_r("email: " . print_r($latest_email) . PHP_EOL);
+
+        print_r("json: " . print_r($json) . PHP_EOL);
+
+        return $json;
     }
 }
